94d6de620d68b67e1f7756e2c709a0351c329e6e
Alexander Bogish <ego.cogitare@gmail.com>
2017-02-11 13:25:39 +0200

    ...

--- src/js/states/SplashScreen.js
+++ src/js/states/SplashScreen.js
@@ -2,13 +2,17 @@ import Config from './../Config';
 import Assets from './../Assets';
 
 export default class {
+
   preload() {
+    this.preloadingStart = Date.now();
     Assets.forEach((asset) => {
       this.game.load[asset.type](...asset.params);
     });
   }
 
   create() {
+    this.assetsLoaded();
+    
     game.stage.backgroundColor = Config.backgroundColor;
     const image = game.add.image(0, 0, 'splash-01');
 
@@ -16,9 +20,18 @@ export default class {
      * Set splash screen
      */
     game.add.tween(image)
-      .to({ alpha: 0 }, 1000, Phaser.Easing.Linear.None, true, 300)
+      .to({ alpha: 0 }, 1000, Phaser.Easing.Linear.None, true, 2000)
       .onComplete.add(() => {
         game.state.start('PlayersCount');
       });
+  }
+
+  assetsLoaded() {
+    console.log(
+        '%c Assets preloaded in '
+        + Number((Date.now() - this.preloadingStart) / 1000)
+        + ' sec',
+        'color: #1256A9; font-weight: bold'
+    );
   }
 };
