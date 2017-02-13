5c48d59414a93829426a831c45cf8c500eeb0715
Alexander <ego.cogitare@gmail.com>
2017-02-13 19:27:34 +0200

    Player parametres info

--- src/config/characters.js
+++ src/config/characters.js
@@ -1,30 +1,66 @@
 export default [
   {
     id: 0,
-    name: 'Snake'
+    name: 'Snake',
+    planet: 'Terra',
+    parametres: {
+      acceleration: 1,
+      topSpeed: 1,
+      jumping: 1
+    }
   },
   {
     id: 12,
-    name: 'Olaf'
+    name: 'Olaf',
+    planet: 'Valhalla',
+    parametres: {
+      acceleration: 2,
+      jumping: 1
+    }
   },
   {
     id: 13,
-    name: 'Tarquinn'
+    name: 'Tarquinn',
+    planet: 'Aurora',
+    parametres: {
+      topSpeed: 1,
+      cornering: 1
+    }
   },
   {
     id: 14,
-    name: 'Jake Badlands'
+    name: 'Jake Badlands',
+    planet: 'Xeno Prime',
+    parametres: {
+      acceleration: 1,
+      cornering: 1
+    }
   },
   {
     id: 15,
-    name: 'Katarina Lyons'
+    name: 'Katarina Lyons',
+    planet: 'Panteros V',
+    parametres: {
+      jumping: 1,
+      cornering: 1
+    }
   },
   {
     id: 16,
-    name: 'Ivanzupher'
+    name: 'Ivanzypher',
+    planet: 'Fleagull',
+    parametres: {
+      jumping: 1,
+      topSpeed: 1
+    }
   },
   {
     id: 17,
-    name: 'Cyberhawk'
+    name: 'Cyberhawk',
+    planet: 'Serpentis',
+    parametres: {
+      acceleration: 1,
+      jumping: 1
+    }
   }
 ];
--- src/js/states/SelectDriver.js
+++ src/js/states/SelectDriver.js
@@ -12,6 +12,17 @@ export default class {
     game.settings.players = game.settings.players || [];
   }
 
+  mapCharacterParam(param) {
+    const paramsMap = {
+      acceleration: 'Acceleration',
+      jumping: 'Jumping',
+      topSpeed: 'Top speed',
+      cornering: 'Cornering',
+    };
+
+    return paramsMap[param];
+  }
+
   create() {
     const leftButton = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
     const rightButton = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
@@ -52,8 +63,14 @@ export default class {
     this.playerDriver = game.add.sprite(40, 355, 'characters');
     this.playerDriver.frame = this.characters[this.characterId].id;
 
-    this.textPlayer = game.add.bitmapText(390, 340, 'font', 'Player 1', 24);
-    this.textCharacterName = game.add.bitmapText(270, 390, 'font', '', 24);
+    this.textPlayer = game.add.bitmapText(390, 340, 'font', 'Player 1', 20);
+    this.textCharacterName = game.add.bitmapText(270, 380, 'font', '', 20);
+    this.textPlanet = game.add.bitmapText(270, 410, 'font', '', 20);
+
+    this.textParams = [];
+    for (let i = 0; i < 3; i++) {
+      this.textParams.push(game.add.bitmapText(270, 525 - i * 25, 'font', '', 20));
+    }
 
     // Show current (default) character
     this.switchCharacter(0);
@@ -82,5 +99,12 @@ export default class {
 
     // Display character information (name, parametres, etc)
     this.textCharacterName.setText(character.name);
+    this.textPlanet.setText(character.planet);
+
+    this.textParams.forEach((paramText) => paramText.setText(''));
+    const charParams = Object.keys(this.characters[this.characterId].parametres);
+    charParams.forEach((param, key) => {
+      this.textParams[key].setText('+1 ' + this.mapCharacterParam(param));
+    });
   }
 };
